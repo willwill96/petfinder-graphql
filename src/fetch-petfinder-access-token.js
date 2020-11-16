a5c68243d00adbf8d43f4024e14e499359bf738f
willwill96 <william.will@connexta.com>
2020-11-16 16:17:24 -0700

    Updates

--- src/fetch-petfinder-access-token.js
+++ src/fetch-petfinder-access-token.js
@@ -3,8 +3,8 @@ import fetch from 'isomorphic-unfetch'
 const fetchAccessToken = async () => {
   const params = new URLSearchParams()
   params.append('grant_type', 'client_credentials')
-  params.append('client_id', process.env.PETFINDER_API_KEY)
-  params.append('client_secret', process.env.PETFINDER_SECRET_KEY)
+  params.append('client_id', process.env['PETFINDER_API_KEY'])
+  params.append('client_secret', process.env['PETFINDER_SECRET_KEY'])
   const petfinderRes = await fetch(
     'https://api.petfinder.com/v2/oauth2/token',
     {
